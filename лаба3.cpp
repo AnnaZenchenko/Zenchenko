a14c726fc07040dd8434b1e0b6fb6b32026e81bb
AnnaZenchenko <72825182+AnnaZenchenko@users.noreply.github.com>
2020-10-20 14:57:14 +0300

    Add files via upload

---
+++ лаба3.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <iostream>
+#define n 4
+int main()
+{
+	int a = 0, b = 0, i, ms[n];
+	printf("fill the array\n");
+	for (i = 0; i < n; i++)
+	{
+		scanf_s("%d", &ms[i]);
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (ms[i] % 5 == 0 && ms[i] % 7 != 0)
+		{
+			a += 1;
+			b = b + ms[i];
+		}
+	}
+	printf("number of numbers needed %d\n", a);
+	printf("the sum of numbers needed %d\n", b);
+	return(0);
+}
