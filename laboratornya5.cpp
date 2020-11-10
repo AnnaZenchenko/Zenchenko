5a12e639dd7b4edf8bb17a699aabef15aaf7d2b9
AnnaZenchenko <72825182+AnnaZenchenko@users.noreply.github.com>
2020-11-10 22:35:47 +0300

    Add files via upload

---
+++ laboratornya5.cpp
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int mr[5][5];
+
+	printf("\n enter array elements\n"); //"введите элементы массива"
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			scanf_s(" %d", &mr[i][j]);
+		}
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		printf("\n");
+		for (int j = 0; j < 5; j++)
+		{
+			printf("%d ", mr[i][j]);
+		}
+	}
+
+	return 0;
+}
