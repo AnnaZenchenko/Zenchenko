54b2f1c86442ae9a1780a9ba07fb7e7a5da46d00
AnnaZenchenko <72825182+AnnaZenchenko@users.noreply.github.com>
2020-11-24 23:42:26 +0300

    Add files via upload

---
+++ laba5.cpp
@@ -0,0 +1,32 @@
+﻿#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+    int *mr, n;
+    scanf_s("%d", &n);//вводим кол-во элементов в матрице
+    mr = (int*)calloc(n * n, sizeof(int)); //calloc-выделяется блок памяти для массива 
+    printf("\n enter array elements\n"); //"введите элементы массива"
+    for (int i = 0; i < n; i++)
+    {
+        scanf_s("%d", mr + i);// ввод массива с экрана 
+    }
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+
+            if (j == n - 1) { *(mr + i * n + j) = *(mr + n * (i - 1)); 
+            }
+
+            else { *(mr + i * n + j) = *(mr + (i - 1) * n + j + 1); }
+        }
+
+    }
+    for (int i = 0; i < n; i++) {
+        printf("\n");
+        for (int j = 0; j < n; j++)
+            printf("%d ", *(mr + i*n + j));
+    }
+    return 0;
+}
